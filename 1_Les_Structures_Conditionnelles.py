487c0c6b204de5c44b309b3f34214934952dbb29
Tini-Cedric-Bonzi <bonzicedric6@gmail.com>
2024-07-09 17:47:40 +0000

    modification

--- 1_Les_Structures_Conditionnelles.py
+++ 1_Les_Structures_Conditionnelles.py
@@ -1,9 +1,9 @@
 # nous allons rediger un programme qui demandera à tout utlisateur d'entrer une note , à la suite de la note entrer il obtiendra une mention #
 
-# nous allons tout d'abord definir une fonction qui permettra à l'utilsateur d'entrer une note entre 0 et 20 y compris des notes decimale #
+# nous allons tout d'abord definir une fonction qui permettra à l'utilsateur d'entrer une note y compris des notes decimale #
 def mention_note():
     note = float(input("Entrez une note: "))
-# nous allons à présent demander à notre programme d'afficher les mentions en fonction des notes recu #
+# nous allons à présent demander à notre programme d'afficher les mentions en fonction des notes entrer #
     if note >= 18:
         print("Excellent")
     elif note >= 16:
